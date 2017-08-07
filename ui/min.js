495d33ccef9416e1d37fab2525b29b428f4ba70b
saurabh19952016 <saurabhshuklas1995@gmail.com>
2017-08-07 22:10:21 +0530

    Create min.js

---
+++ ui/min.js
@@ -0,0 +1,7 @@
+console.log('Loaded!');
+var img=document.getElementById('mado');
+img.onclick =function(){
+    img.style.marginLeft='100px';
+};
+var ele=document.getElementById('main-text');
+ele.innerHTML='New';
