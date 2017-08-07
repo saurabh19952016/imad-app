29d198152f83a535f15bf410970b9103ba81ab8e
saurabh19952016 <saurabhshuklas1995@gmail.com>
2017-08-07 22:02:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,5 @@
 console.log('Loaded!');
-var element=document.getElementById('madi');
-element.innerHTML="Hello";
+var element=document.getElementById('mado');
+element.onclick =function(){
+    element.style.marginLeft='100px';
+};
