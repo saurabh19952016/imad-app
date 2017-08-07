717dca830e64211eb0b86e8af199a9ae62c0d88d
saurabh19952016 <saurabhshuklas1995@gmail.com>
2017-08-07 22:09:58 +0530

    Delete main.js

--- ui/main.js
+++
@@ -1,7 +0,0 @@
-console.log('Loaded!');
-var img=document.getElementById('mado');
-img.onclick =function(){
-    img.style.marginLeft='100px';
-};
-var ele=document.getElementById('main-text');
-ele.innerHTML='New';
